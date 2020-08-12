9cfc913436cdc66869ac845f28c317b6334c107b
Everton Toffanetto <contato@querocriarsite.com>
2020-08-12 17:55:30 -0300

    ajustes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,74 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 import Auto from "../../components/Auto";
 import Circle from "../../components/Circle";
 import Day from "../../components/Day";
 import Control from "../../components/Control";
 import Footer from "../../components/Footer";
 import City from "../../components/City";
-import Climate from "../../API";
+import axios from 'axios';
+import Key_api from "../../API_KEY";
+import {Alert} from "./styles";
 
 const Home = () => {
-    return <>
-        <Climate/>
+    const [location, setLocation] = useState(false);
+    const [weather, setWeather] = useState(false);
 
-        <City/>
+    const getWeather = async (lat, long) => {
+        const res = await axios.get("https://api.hgbrasil.com/weather", {
+            params: {
+                format: 'json-cors',
+                locale: 'pt',
+                lat: lat,
+                lon: long,
+                user_ip: 'remote',
+                key: Key_api,
+            }
+        });
+        setWeather(res.data);
+        console.log(res.data)
+    }
 
-        <Day/>
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            getWeather(position.coords.latitude, position.coords.longitude);
+            setLocation(true)
+        })
+    }, []);
 
-        <Circle/>
+    if (location === false) {
+        return <Alert>
+            Você precisa habilitar a localização no navegador!
+        </Alert>;
+    } else if (weather === false) {
+        return <Alert>
+            Carregando...
+        </Alert>;
+    } else {
+        return <>
+            <ul>
+                <li>Cidade: {}</li>
+                <li>Dia: {}</li>
+                <li>Temperatura atual: {}°</li>
+                <li>Temperatura máxima: {}°</li>
+                <li>Temperatura minima: {}°</li>
+                <li>Velocidade do vento: {}km/h</li>
+                <li>Humidade: {}%</li>
+                <li>Previsão: {}</li>
+            </ul>
 
-        <Control/>
+            <City/>
 
-        <Auto/>
+            <Day/>
 
-        <Footer/>
-    </>;
+            <Circle/>
+
+            <Control/>
+
+            <Auto/>
+
+            <Footer/>
+        </>;
+    }
 };
 
 export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Alert = styled.div`
+    display: flex;
+    padding: 20px;
+    text-align: center;
+    font-size: 30px;
+`;
